dd5f01fbb670aafd9e8f11d7414c3c5c39a2f327
pl3lee <billylee123@gmail.com>
2022-01-24 22:19:13 -0500

    Added a hover effect. Wrapped javascript commands inside a function.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const grid = document.querySelector('.grid');
-const rowArray = []
-for (let row = 0; row < 16; row++) {
-    let gridRow = document.createElement('div');
-    gridRow.style.cssText = 'flex: 1; width: 100vh; display: flex;';
-    gridRow.classList.add('row');
-    for (let col = 0; col < 16; col++) {
-        let gridElement = document.createElement('div');
-        gridElement.style.cssText = 'flex: 1; border: solid #808080; border-width: 1px';
-        // gridElement.textContent = 'hello'
-        gridRow.appendChild(gridElement);
+function createGrid() {
+    const grid = document.querySelector('.grid');
+    const rowArray = []
+    for (let row = 0; row < 16; row++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('row');
+        for (let col = 0; col < 16; col++) {
+            let gridElement = document.createElement('div');
+            gridElement.classList.add('gridBox')
+            gridRow.appendChild(gridElement);
+        }
+        rowArray.push(gridRow);
     }
-    rowArray.push(gridRow);
+    rowArray.forEach(rowElement => grid.appendChild(rowElement));
 }
-rowArray.forEach(rowElement => grid.appendChild(rowElement));
+
+createGrid();
